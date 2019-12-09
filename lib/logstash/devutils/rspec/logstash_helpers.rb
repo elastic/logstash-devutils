b4d47833ca75dfe32b56db0552436d8aedbf7abb
Karol Bucek <self@kares.org>
2019-12-09 18:20:51 +0100

    Deprecate: kill plugin_input helper

--- lib/logstash/devutils/rspec/logstash_helpers.rb
+++ lib/logstash/devutils/rspec/logstash_helpers.rb
@@ -108,17 +108,9 @@ module LogStashHelper
     result
   end
 
+  # @deprecated
   def plugin_input(plugin, &block)
-    queue = Queue.new
-
-    input_thread = Thread.new do
-      plugin.run(queue)
-    end
-    result = block.call(queue)
-
-    plugin.do_stop
-    input_thread.join
-    result
+    raise NotImplementedError.new("#{__method__} no longer supported; please refactor")
   end
 
   def agent(&block)
