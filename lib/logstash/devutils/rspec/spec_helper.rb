cc5940eea696c48238c02c38f0623cbdbb67cfa5
Karol Bucek <self@kares.org>
2019-12-10 10:53:15 +0100

    Review: use an around rspec hook for metadata

--- lib/logstash/devutils/rspec/spec_helper.rb
+++ lib/logstash/devutils/rspec/spec_helper.rb
@@ -48,8 +48,9 @@ RSpec.configure do |config|
 
   config.filter_run_excluding LogStashHelper.excluded_tags
 
-  config.before(:each) do |example|
+  config.around(:each) do |example|
     @__current_example_metadata = example.metadata
+    example.run
   end
 
   # Run specs in random order to surface order dependencies. If you find an
